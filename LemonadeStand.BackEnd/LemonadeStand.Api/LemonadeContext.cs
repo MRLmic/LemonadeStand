befabb69c89102939903967924dd3a2038c2d8a3
Melissa Luna <missi.luna@gmail.com>
2024-04-23 23:44:16 -0400

    Replace context with ef scaffolded context

--- LemonadeStand.BackEnd/LemonadeStand.Api/LemonadeContext.cs
+++
@@ -1,8 +0,0 @@
-public class LemonadeContext : DbContext
-    {
-        public LemonadeContext(DbContextOptions<Test1DbContext> options)
-            : base(options)
-        {
-        }
-        public DbSet<Customer> Customers { get; set; }
-    }
---
+++ LemonadeStand.BackEnd/LemonadeStand.Api/Models/LemonadeContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LemonadeStand.Models;
+
+public partial class LemonadeContext : DbContext
+{
+    public LemonadeContext()
+    {
+    }
+
+    public LemonadeContext(DbContextOptions<LemonadeContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("lemonade"));
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
