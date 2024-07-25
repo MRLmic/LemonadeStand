0579877a20356482e86b8e730d93dfadd0f47a70
Melissa Luna <missi.luna@gmail.com>
2024-07-25 13:04:04 -0400

    Include product seeding query

---
+++ LemonadeStand.BackEnd/LemonadeStand.Api/Queries/insertProducts.sql
@@ -0,0 +1,6 @@
+INSERT INTO "Products" ("ProductId", "Flavor", "Size", "Price")
+VALUES
+    (1,'Pink', 'Regular', 1.5), 
+    (2,'Pink', 'Large', 1.5),
+    (3,'Regular', 'Regular', 1),
+    (4,'Regular', 'Large', 1.5);
