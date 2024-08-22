a316cae5b1b2d69578301b4a75095f0227de3f9e
Melissa Luna <missi.luna@gmail.com>
2024-08-22 18:11:03 -0400

    Adjust properties of products and map

--- LemonadeStand.Client/src/components/ItemDisplay/ListContainer.jsx
+++ LemonadeStand.Client/src/components/ItemDisplay/ListContainer.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import ListRow from './ListRow.jsx';
 
-interface ListContainerProps {
-    orderTypes: OrderType[]
-}
-
-interface OrderType {
-    itemName: string;
-    flavor: string;
-    size: string;
-  }
-
-const ListContainer: React.FC<ListContainerProps> = ({orderTypes}) => {
+const ListContainer = (props) => {
     return (
         <div className='List-container'>
             <table>
@@ -23,7 +13,7 @@ const ListContainer: React.FC<ListContainerProps> = ({orderTypes}) => {
                         <th className='qty-header'>QTY</th>
                         <th className='total-header'>Total</th>
                     </tr>
-                    {orderTypes.map((type: OrderType, index: number) => <ListRow key={index} itemName={type.itemName} flavor={type.flavor} size={type.size} price={type.size === "Regular" ? 1 : 1.5}></ListRow>)}
+                    {props.products.map((product, index) => <ListRow key={index} flavor={product.flavor} size={product.size} price={product.price}></ListRow>)}
                 </tbody>
             </table>
         </div>
