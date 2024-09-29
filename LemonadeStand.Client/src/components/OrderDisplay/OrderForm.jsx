b35af2b4307d13b9e6afe9d5a7ec9526c060d40a
Melissa Luna <missi.luna@gmail.com>
2024-09-28 21:54:24 -0400

    Convert OrderForm from utilizing useRef to a controlled component

--- LemonadeStand.Client/src/components/OrderDisplay/OrderForm.jsx
+++ LemonadeStand.Client/src/components/OrderDisplay/OrderForm.jsx
@@ -1,51 +1,81 @@
-import React, {useContext, useEffect, useRef} from 'react';
-import { OrderContext } from '../../components/Wrapper.jsx';
+import React, { useContext, useEffect, useState } from "react";
+import { OrderContext } from "../../components/Wrapper.jsx";
 
-export const OrderForm = ({open,setOpen,submit}) => {
-    const phoneNumRef = useRef(null);
-    const emailRef = useRef(null);
-    const nameRef = useRef(null);
-    const {dispatch} = useContext(OrderContext);
+export const OrderForm = ({ open, setOpen, submit }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const { dispatch } = useContext(OrderContext);
 
   useEffect(() => {
     if (submit) {
-        if (phoneNumRef.current?.value === '' && emailRef.current?.value === '') {
-            alert('An email or phone number is required to place an order.')
-            return;
+      if (name === "") {
+        alert("Please enter a name for this order");
+        return;
+      } else if (phone === "" && email === "") {
+        alert(
+          "Please enter a phone number or email so we can notify you when your order is ready"
+        );
+        return;
+      } else {
+        let customerInfo;
+        if (phone) {
+          customerInfo = { customerName: name, phone: phone };
         } else {
-            let customerContact;
-            let contactType;
-            if (emailRef.current?.value === '') {
-                customerContact = phoneNumRef.current?.value
-                contactType = 'phone'
-            } else {
-                customerContact = emailRef.current?.value
-                contactType = 'email'
-            }
-            dispatch({type: 'SUBMIT', orderName: nameRef.current?.value, customerContact, contactType})
-            setOpen(false)
+          customerInfo = { customerName: name, email: email };
         }
+        dispatch({ type: "SUBMIT", customerInfo: customerInfo });
+        setOpen(false);
+      }
     }
-}, [submit, dispatch, setOpen])
+  }, [submit, name, phone, email, dispatch, setOpen]);
 
-    if (open) { 
-        
-    return (<form className="OrderForm">
-                <span>Please enter your name and your phone number OR email and click submit to confirm!</span><br></br>
-                <label>
-                Name:
-                <input required autoFocus type="text" name="name" ref={nameRef}/>
-                </label><br></br>
-                <label>
-                Phone Number (no more than 10 numbers):
-                <input type="tel" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" maxLength={10} name="phone number" ref={phoneNumRef}/>
-                </label><br></br>
-                <label>
-                Email:
-                <input type="email" name="email" ref={emailRef} />
-                </label>
-           </form>) 
-    } else {return ""};   
-}
+  if (open) {
+    return (
+      <form className="OrderForm">
+        <span>
+          Please enter your name and your phone number OR email and click submit
+          to confirm!
+        </span>
+        <br></br>
+        <label>
+          Name:
+          <input 
+            required
+            autoFocus 
+            type="text" 
+            name="name" 
+            value={name} 
+            onChange={(e) => setName(e.target.value)}
+            />
+        </label>
+        <br></br>
+        <label>
+          Phone Number (no more than 10 numbers):
+          <input
+            type="tel"
+            pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+            maxLength={10}
+            name="phone number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <label>
+          Email:
+          <input 
+            type="email" 
+            name="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+            />
+        </label>
+      </form>
+    );
+  } else {
+    return "";
+  }
+};
 
 export default OrderForm;
