cc9f274f1fa62bd7709b3ff21f35cbadd72596aa
Melissa Luna <missi.luna@gmail.com>
2024-09-28 22:40:30 -0400

    Validate an order is not empty before submitting

--- LemonadeStand.Client/src/components/OrderDisplay/TotalBox.jsx
+++ LemonadeStand.Client/src/components/OrderDisplay/TotalBox.jsx
@@ -1,26 +1,36 @@
-import React, { useContext, useState } from 'react';
-import { OrderContext } from '../Wrapper.jsx';
-import { formatter } from '../../utils.js';
-import OrderForm from './OrderForm.jsx';
+import React, { useContext, useState } from "react";
+import { OrderContext } from "../Wrapper.jsx";
+import { formatter } from "../../utils.js";
+import OrderForm from "./OrderForm.jsx";
 
 const TotalBox = () => {
-    const [open, setOpen] = useState(false);
-    const [submit, setSubmit] = useState(0);
-    const { state } = useContext(OrderContext);
-    let orderText = open ? 'Submit' : 'Order Now';
+  const [open, setOpen] = useState(false);
+  const [submit, setSubmit] = useState(0);
+  const { state } = useContext(OrderContext);
+  let orderText = open ? "Submit" : "Order Now";
 
-    const openForm = () => setOpen(true);
-    const submitForm = () => setSubmit((submit) => submit + 1);   
+  const openForm = () => {
+    if (state.total) {
+      setOpen(true);
+    } else {
+      alert(
+        "It looks like you haven't ordered anything yet! Please add some items to your order before submitting."
+      );
+    }
+  };
+  const submitForm = () => setSubmit((submit) => submit + 1);
 
-    return (<div className='total-box'>
-                <div className='total'>Total</div>
-                    <div className='amount'>
-                            {formatter.format(state.total)}
-                    </div>
-                <br></br>
-                <button onClick={open ? submitForm : openForm} className='order-button'>{orderText}</button>
-                <OrderForm open={open} setOpen={setOpen} submit={submit}></OrderForm>
-            </div>);
+  return (
+    <div className="total-box">
+      <div className="total">Total</div>
+      <div className="amount">{formatter.format(state.total)}</div>
+      <br></br>
+      <button onClick={open ? submitForm : openForm} className="order-button">
+        {orderText}
+      </button>
+      <OrderForm open={open} setOpen={setOpen} submit={submit}></OrderForm>
+    </div>
+  );
 };
 
 export default TotalBox;
