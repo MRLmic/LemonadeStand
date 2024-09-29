61a9221aad41a1874706798765014757cd7c7d6c
Melissa Luna <missi.luna@gmail.com>
2024-09-28 22:41:01 -0400

    construct POST payload on submit

--- LemonadeStand.Client/src/components/Wrapper.jsx
+++ LemonadeStand.Client/src/components/Wrapper.jsx
@@ -1,77 +1,119 @@
-import React, { useState, useEffect, useReducer, createContext } from 'react';
-import ListContainer from './ItemDisplay/ListContainer';
-import TotalBox from './OrderDisplay/TotalBox';
+import React, { useState, useEffect, useReducer, createContext } from "react";
+import ListContainer from "./ItemDisplay/ListContainer";
+import TotalBox from "./OrderDisplay/TotalBox";
 
 export const OrderContext = createContext();
 
 const initialState = {};
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_INITIAL_STATE':
-            const newState = { ...state, ...action.payload, total: 0 };
-            return newState;
-        case 'INCREMENT':
-            return {
-                ...state,
-                [action.productId]: (state[action.productId] || 0) + 1,
-                total: state.total + action.price
-            };
-        case 'DECREMENT':
-            return {
-                ...state,
-                [action.productId]: Math.max((state[action.productId] || 0) - 1, 0),
-                total: Math.max(state.total - action.price, 0)
-            };
-        case 'RESET':
-            return {
-                    ...state,
-                    [action.productId]: 0,
-                    total: Math.max(state.total - (state[action.productId] * action.price), 0)
-                };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "SET_INITIAL_STATE":
+      const newState = { ...state, ...action.payload, total: 0 };
+      return newState;
+    case "INCREMENT":
+      return {
+        ...state,
+        [action.productId]: (state[action.productId] || 0) + 1,
+        total: state.total + action.price,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        [action.productId]: Math.max((state[action.productId] || 0) - 1, 0),
+        total: Math.max(state.total - action.price, 0),
+      };
+    case "RESET":
+      return {
+        ...state,
+        [action.productId]: 0,
+        total: Math.max(
+          state.total - state[action.productId] * action.price,
+          0
+        ),
+      };
+    case "SUBMIT":
+      const orderItems = mapStateToOrderItems(state);
+      let order = { total: state.total, orderItems: orderItems };
+      postData(action.customerInfo, order);
+      return {
+        ...initialState,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const postData = (customerInfo, order) => {
+  fetch("api/Orders", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      customerInfo: customerInfo,
+      order: order,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const orderId = data.orderId;
+      alert(`Thank you! Your Order ID is ${orderId}`);
+    })
+    .catch((err) => console.log(err));
+};
+
+const mapStateToOrderItems = (state) => {
+  return Object.entries(state)
+    .filter(([key, value]) => !isNaN(key) && value > 0) // Filter out non-numerical keys
+    .map(([productId, quantity]) => ({
+      productId: Number(productId),
+      quantity: quantity,
+    }));
 };
 
 const Wrapper = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [products, setProducts] = useState([]);
-    const [total, setTotal] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [products, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(process.env.REACT_APP_API_URL, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-                const data = await response.json();
-                setProducts(data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(process.env.REACT_APP_API_URL, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+        setProducts(data);
 
-                const initialStateFromProducts = data.reduce((acc, product) => {
-                    acc[product.productId] = 0; 
-                    return acc;
-                }, {});
+        const initialStateFromProducts = data.reduce((acc, product) => {
+          acc[product.productId] = 0;
+          return acc;
+        }, {});
 
-                dispatch({ type: 'SET_INITIAL_STATE', payload: initialStateFromProducts });
-            } catch (error) {
-                console.log("error", error);
-            }
-        };
+        dispatch({
+          type: "SET_INITIAL_STATE",
+          payload: initialStateFromProducts,
+        });
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <OrderContext.Provider value={{ state, dispatch }}>
-            <div>
-                <ListContainer products={products}></ListContainer>
-                <TotalBox total={total}></TotalBox>
-            </div>
-        </OrderContext.Provider>
-    );
+  return (
+    <OrderContext.Provider value={{ state, dispatch }}>
+      <div>
+        <ListContainer products={products}></ListContainer>
+        <TotalBox total={total}></TotalBox>
+      </div>
+    </OrderContext.Provider>
+  );
 };
 
 export default Wrapper;
